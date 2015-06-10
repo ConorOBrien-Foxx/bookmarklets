91b3bdbfa0755f0edfc458d377fc3e6fae083528
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2015-06-09 22:48:22 -0400

    Update one.js

--- one.js
+++ one.js
@@ -4,6 +4,7 @@ app.id = "ConorOBrienFoxxApp";
 var _my_style = document.createElement("STYLE");
 _my_style.type="text/css";
 _my_style.innerHTML = "#ConorOBrienFoxxApp{top: 10px; right: 10px; opacity: 0.1; background: red; width: 350px; position: fixed;}#ConorOBrienFoxxApp:hover{opacity:1}";
+document.getElementsByTagName('head')[0].appendChild(_my_style);
 document.body.appendChild(app);
 
 /*
