50a4ed0b1b2aad8809b7a756dd15d72e8e00312d
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2015-06-09 23:03:22 -0400

    Update one.js

--- one.js
+++ one.js
@@ -1,10 +1,6 @@
 var app        = document.createElement("div");
 app.innerHTML  = "Testing";
 app.id         = "ConorOBrienFoxxApp";
-var _my_style  = document.createElement("link");
-_my_style.rel  = "STYLESHEET";
-_my_style.href = "https://raw.githubusercontent.com/ConorOBrien-Foxx/bookmarklets/gh-pages/one.css";
-document.getElementsByTagName('head')[0].appendChild(_my_style);
 document.body.appendChild(app);
 
 /*
@@ -14,5 +10,22 @@ javascript:(function(){
   _my_script.src='https://raw.githubusercontent.com/ConorOBrien-Foxx/bookmarklets/gh-pages/one.js';
   document.getElementsByTagName('head')[0].appendChild(_my_script);
 })();
+<!DOCTYPE html>
+<html>
+
+<body>
+<a href="javascript:(function(){
+  _my_script=document.createElement('SCRIPT');
+  _my_script.type='text/javascript';
+  _my_script.src='https://raw.githubusercontent.com/ConorOBrien-Foxx/bookmarklets/gh-pages/one.js';
+  document.getElementsByTagName('head')[0].appendChild(_my_script);
+var _my_style  = document.createElement("link");
+_my_style.rel  = "STYLESHEET";
+_my_style.href = "https://raw.githubusercontent.com/ConorOBrien-Foxx/bookmarklets/gh-pages/one.css";
+document.getElementsByTagName('head')[0].appendChild(_my_style);
+})()">bookmarklet</a>
+</body>
+
+</html>
 
 */
