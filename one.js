44b0ecfca41ea836d0308ede42ad509ad4ea660f
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2015-06-09 22:40:45 -0400

    Update one.js

--- one.js
+++ one.js
@@ -1,14 +1,19 @@
 var app = document.createElement("div");
 app.innerHTML = "Testing";
 app.style = "top: 10px; right: 10px; opacity: 0.1; background: red; width: 350px; position: fixed;";
-
 document.body.appendChild(app);
+app.addEventListener("mouseover",function(){
+  this.style.opacity = "1";
+});
+app.addEventListener("mouseout",function(){
+  this.style.opacity = "0.1";
+});
 
 /*
 javascript:(function(){
   _my_script=document.createElement('SCRIPT');
   _my_script.type='text/javascript';
-  _my_script.src='http://mysite.com/script.js?';
+  _my_script.src='https://raw.githubusercontent.com/ConorOBrien-Foxx/bookmarklets/gh-pages/one.js';
   document.getElementsByTagName('head')[0].appendChild(_my_script);
 })();
 
